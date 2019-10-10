8a184dd4be34bbf04a54fb96925c45789bea0ea3
jmendel98 <55898769+jmendel98@users.noreply.github.com>
2019-10-10 14:42:57 -0700

    Update rpi_webcam.py

--- rpi_webcam.py
+++ rpi_webcam.py
@@ -1,9 +1,9 @@
 import cv2
 print('setting up webcam')
-#camera = cv2.VideoCapture(0)
-#for i in range(1):
- #   return_value, image = camera.read()
-  #  cv2.imwrite('opencv'+str(i)+'.png', image)
-#del(camera)
+camera = cv2.VideoCapture(0)
+for i in range(1):
+    return_value, image = camera.read()
+    cv2.imwrite('opencv'+str(i)+'.png', image)
+del(camera)
 
 print('I took a picture with the webcam!')
