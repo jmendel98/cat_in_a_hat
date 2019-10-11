735ad9808d3993ed387b3bed9284526ca231d693
ericsarnoff <55898750+ericsarnoff@users.noreply.github.com>
2019-10-10 19:11:11 -0700

    Lab3

--- rpi_webcam.py
+++ rpi_webcam.py
@@ -1,9 +1,7 @@
 import cv2
-print('setting up webcam')
+print("Smile!")
 camera = cv2.VideoCapture(0)
 for i in range(1):
     return_value, image = camera.read()
     cv2.imwrite('opencv'+str(i)+'.png', image)
 del(camera)
-
-print('I took a picture with the webcam!')
