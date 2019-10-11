7df669edcd2646d445bde5018fc9b3406e81ee8c
Jesse <jcqwang@gmail.com>
2019-10-11 19:53:47 +0000

    Jesse - lab 2

--- capture.py
+++ capture.py
@@ -1,11 +1,11 @@
 import cv2
+import numpy as np
 
-cam = cv2.videoCapture(0)
+cam = cv2.VideoCapture(0)
 img_counter = 0
+ret, frame = cam.read()
 img_name = "opencv_frame_{}.png".format(img_counter)
 cv2.imwrite(img_name, frame)
 print("{} written!".format(img_name))
 
-cam.release()
-
-cv2.destroyAllWindows()
+del(cam)
